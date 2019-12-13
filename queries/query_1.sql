30bbfd5c3836ddd475f1626ea5196b3d7978a61f
ArtDub <rusartdub@gmail.com>
2019-12-14 01:26:11 +0300

    report begin

--- queries/query_1.sql
+++ queries/query_1.sql
@@ -10,7 +10,7 @@ SELECT marks_list.id_student, marks_list.mark, _exam.id_exam, _exam.name as exam
   INTO CURSOR _marks
 -- Берем номер группы у студентов и их имена
 SELECT students.name, marks.id_exam, students.id_group, _marks.exam_name
-  FROM students, marks
+  FROM students, _marks
   WHERE students.id_student = _marks.id_student
   INTO CURSOR _students
 -- Берем пару студентов с разными именами
