3c5149b140eb0ac787cc9dc6a23e7ba4e70a9c4c
ArtDub <rusartdub@gmail.com>
2019-12-23 15:18:23 +0300

    stable version

--- cp/quiries/query.sql
+++ cp/quiries/query.sql
@@ -1,8 +1,21 @@
 -- Распечатать ведомость по выбранному предмету
-SELECT id_subject FROM subjects WHERE name = "Algorithms" INTO CURSOR _subject
-SELECT exams.id_exam FROM exams, _subject WHERE exams.id_subject = _subject.id_subject INTO CURSOR _exam
-SELECT marks_list.id_student, marks_list.mark FROM marks_list, _exam WHERE marks_list.id_exam = _exam.id_exam INTO CURSOR _marks
-SELECT students.name, _marks.mark FROM _marks, students WHERE _marks.id_student = students.id_student
+
+SELECT id_subject, name AS subj_name FROM subjects;
+WHERE name = subject_val;
+INTO CURSOR _subject
+
+SELECT exams.id_exam,  FROM exams, _subject;
+WHERE exams.id_subject = _subject.id_subject;
+INTO CURSOR _exam
+
+SELECT marks_list.id_student, marks_list.mark FROM marks_list, _exam;
+WHERE marks_list.id_exam = _exam.id_exam;
+INTO CURSOR _marks
+
+SELECT students.name AS Èìÿ, _marks.mark AS Îöåíêà FROM _marks, students;
+WHERE _marks.id_student = students.id_student;
+INTO TABLE group_report
+
 -- Поиск студентов с двойкой
 SELECT id_subject FROM subjects WHERE name = "Algorithms" INTO CURSOR _subject
 SELECT exams.id_exam FROM exams, _subject WHERE exams.id_subject = _subject.id_subject AND ((year(Data) = 2019 AND month(Data) >= 9) OR (year(Data) = 2020 AND month(Data) <2)) INTO CURSOR _exam
